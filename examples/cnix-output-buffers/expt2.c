ec3da0047d37abd15a877b75d00ec40d6ca51976
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2017-01-28 22:13:43 -0600

    Add examples for the output-buffers essay.

---
+++ examples/cnix-output-buffers/expt2.c
@@ -0,0 +1,34 @@
+/**
+ * expt1
+ *   A simple experiment with pointers and dereferences.
+ *
+ * <insert FOSS license>
+ */
+
+// +---------+-------------------------------------------------------
+// | Headers |
+// +---------+
+
+#include <stdio.h>
+#include <malloc.h>
+
+// +-----------+-----------------------------------------------------
+// | Constants |
+// +-----------+
+
+// The longest string length
+#define MAXLEN 32
+
+// +------+----------------------------------------------------------
+// | Main |
+// +------+
+
+int
+main (int argc, char *argv[])
+{
+  char *ptr;
+  printf ("Assigning to element 0 of %p ... ", ptr);
+  *ptr = 'a';
+  printf ("done\n");
+  return 0;
+} // main
---
+++ examples/cnix-output-buffers/v0.c
@@ -0,0 +1,22 @@
+/**
+ * expt1
+ *   A simple experiment with pointers and dereferences.
+ *
+ * <insert FOSS license>
+ */
+
+// +---------+-------------------------------------------------------
+// | Headers |
+// +---------+
+
+#include <stdio.h>
+#include <malloc.h>
+
+// +------+----------------------------------------------------------
+// | Main |
+// +------+
+
+int
+main (int argc, char *argv[])
+{
+} // main
